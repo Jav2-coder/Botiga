553ce5ed222d0036f24e13b1297d5545e8254088
Surrui <Surrui@HAL9000>
2016-05-16 21:11:31 +0200

    Commit: Carrusel arreglado

--- Botiga/src/main/java/net/javierjimenez/Controllers/BotigaController.java
+++ Botiga/src/main/java/net/javierjimenez/Controllers/BotigaController.java
@@ -289,11 +289,13 @@ public class BotigaController {
 	}
 
 	private double round(double value, int places) {
+		
 		if (places < 0)
 			throw new IllegalArgumentException();
 
 		BigDecimal bd = new BigDecimal(value);
 		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		
 		return bd.doubleValue();
 	}
 }
