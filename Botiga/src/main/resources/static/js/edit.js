d7321451f5823f68a57a3199543385a005cce1eb
Surrui <Surrui@HAL9000>
2016-05-18 22:39:47 +0200

    Commit: Problema con JQuery

--- Botiga/src/main/resources/static/js/edit.js
+++ Botiga/src/main/resources/static/js/edit.js
@@ -4,17 +4,25 @@ jQuery(document).ready(function(){
 		
 		e.preventDefault();
 		
-		var nombre = $(this).closest("tr").children(":nth-child(1)").text();
-		var precio = null;
-		var genero = null;
-		var distribuidora = null;
-		var plataforma = null;
-		var edad = null;
-		var cantidad = null;
-		var imagen = null;
-		var activar = null;
+		var nombre = $(this).closest("tr").children(":nth-child(1)").html();
+		var precio = $(this).closest("tr").children(":nth-child(8)").html();
+		var genero = $(this).closest("tr").children(":nth-child(2)").html();
+		var distribuidora = $(this).closest("tr").children(":nth-child(3)").html();
+		var plataforma = $(this).closest("tr").children(":nth-child(4)").html();
+		var edad = $(this).closest("tr").children(":nth-child(5)").html();
+		var cantidad = $(this).closest("tr").children(":nth-child(6)").html();
+		var activar = $(this).closest("tr").children(":nth-child(7)").html();
 		
-		console.log(nombre);
+		console.log(nombre)
+		
+		$("#nom_juego").append(nombre);
+		$("#pre_juego").append(precio);
+		$("#generos").append(genero);
+		$("#distribuidoras").append(distribuidora);
+		$("input#pre_juego").append(precio);
+		$("input#pre_juego").append(precio);
+		$("input#pre_juego").append(precio);
+		$("input#pre_juego").append(precio);
 		
 	});
 });
